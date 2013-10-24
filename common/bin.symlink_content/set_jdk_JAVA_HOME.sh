055c0df433ff54ef7c41c967d1047fc6f1aa0b9a
Darcy Parker <darcyparker@gmail.com>
2013-10-24 10:46:07 -0400

    spelling error fixed

--- common/bin.symlink_content/set_jdk_JAVA_HOME.sh
+++ common/bin.symlink_content/set_jdk_JAVA_HOME.sh
@@ -18,7 +18,7 @@ if [ -z $JAVA_HOME ]; then
       #readlink where -f has different meaning)
       #Sometimes java is pointing to JRE inside a JDK
       # - if so, JAVA_HOME is set to the JDK root folder
-      # - sotherwise, JAVA_HOME is just 2 folders up from java command
+      # - otherwise, JAVA_HOME is just 2 folders up from java command
       export JAVA_HOME=`readlink -f $(which java) | sed 's:\(/jre\)\?/bin/java$:/:'`
       ;;
     MINGW*)
