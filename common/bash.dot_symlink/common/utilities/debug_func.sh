7568072c9d1c9c05be54045044e66a9294ceb20d
Darcy Parker <darcyparker@gmail.com>
2014-12-04 14:37:08 -0500

    Reworked debug funcs for bsd and gnu

--- common/bash.dot_symlink/common/utilities/debug_func.sh
+++ common/bash.dot_symlink/common/utilities/debug_func.sh
@@ -1,19 +1,24 @@
 #!/usr/bin/env bash
 . $_utilitiesDir/time_func.sh
 
-_lastDebugTime=$(date +"%H:%M:%S.%3N")
+if ! declare -f _debug &>/dev/null ; then
+  function _debugInitTime() {
+    _lastDebugTime=$(getTimeStamp)
+    if [ -n "$_debug" ]; then
+      echo "${_lastDebugTime}:StartTime"
+    fi
+  }
 
-function _debugInitTime() {
-  _lastDebugTime=$(date +"%H:%M:%S.%3N")
-}
-
-function _debug() {
-  local _currentTime=$(date +"%H:%M:%S.%3N")
-  local _startms=$(ts_get_msec $_lastDebugTime)
-  local _endms=$(ts_get_msec $_currentTime)
-  local _timediff=$((10#$_endms - 10#$_startms))ms
-  if [ -n "$_debug" ]; then
-    echo "${_currentTime}:timeDiff=${_timediff}: "$@""
-  fi
-  _lastDebugTime=$_currentTime
-}
+  function _debug() {
+    local _currentTime=$(date +"%H:%M:%S.%3N")
+    local _startms=$(ts_get_msec $_lastDebugTime)
+    local _endms=$(ts_get_msec $_currentTime)
+    local _timediff=$((10#$_endms - 10#$_startms))ms
+    if [ -n "$_debug" ]; then
+      echo "${_currentTime}:timeDiff=${_timediff}: "$@
+    fi
+    _lastDebugTime=$_currentTime
+  }
+  export -f _debugInitTime
+  export -f _debug
+fi
--- common/bash.dot_symlink/common/utilities/time_func.sh
+++ common/bash.dot_symlink/common/utilities/time_func.sh
@@ -1,7 +1,32 @@
 #!/usr/bin/env bash
 
-#echo $(((10#$h*60*60*1000)+(10#$m*60*1000)+(10#$s*1000)+10#$ms))
-function ts_get_msec() {
-  read -r h m s ms <<< $(echo $1 | tr '.:' ' ' )
-  echo $(((10#$h*3600000)+(10#$m*60000)+(10#$s*1000)+10#$ms))
-}
+if ! declare -f getTimeStamp &>/dev/null ; then
+  if date --version >/dev/null 2>&1 ; then
+    #Has GNU Date
+    if [ -n "$_debug" ]; then
+      echo Has GNU Date
+    fi
+    function getTimeStamp() {
+      date +"%H:%M:%S.%3N"
+    }
+  else
+    #Does not have GNU Date. (Likely BSD)
+    if [ -n "$_debug" ]; then
+      echo Does not have GNU Date
+    fi
+    function getTimeStamp() {
+      #Note: BSD Date does not support %3N
+      #      So measurement is not as good.
+      date +"%H:%M:%S.000"
+    }
+  fi
+  export -f getTimeStamp
+fi
+
+if ! declare -f ts_get_msec &>/dev/null ; then
+  function ts_get_msec() {
+    read -r h m s ms <<< $(echo $1 | tr '.:' ' ' )
+    echo $(((10#$h*3600000)+(10#$m*60000)+(10#$s*1000)+10#$ms))
+  }
+  export -f ts_get_msec
+fi
