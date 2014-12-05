efb5d6d3c0b8f2c6c55bb8f91653c8b3e590ecf6
Darcy Parker <dparker@gmail.com>
2014-12-05 10:27:39 -0500

    updates to hpcalc func for cygwin/mingwsys

--- common/bash.dot_symlink/platform/cygwin/bash.2.d/hpcalcFunc.sh
+++ common/bash.dot_symlink/platform/cygwin/bash.2.d/hpcalcFunc.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 #Start HP48GX emulator
-#Does not work unless CWD is in program files
+#For cygwin
 if ! declare -f hpcalc &>/dev/null ; then
   if [ -e /cygdrive/c/Program\ Files\ \(x86\)/HP-Emulators/Emu48/Emu48.vbs -a -e /cygdrive/d/users/dparker/Documents/Hp48gx-darcy.E48 ]; then
     function hpcalc {
-      pushd . > /dev/null
-      cd /cygdrive/c/Program\ Files\ \(x86\)/HP-Emulators/Emu48
-      cyg-wrapper.sh `which cscript` --cyg-verbose=2 /cygdrive/c/Program\ Files\ \(x86\)/HP-Emulators/Emu48/Emu48.vbs /cygdrive/d/users/dparker/Documents/Hp48gx-darcy.E48 > /dev/null
-      popd > /dev/null
+      `which cscript` 'c:\Program Files (x86)\HP-Emulators\Emu48\Emu48.vbs' 'd:\users\dparker\Documents\Hp48gx-darcy.E48' > /dev/null
     }
     export -f hpcalc
   fi
