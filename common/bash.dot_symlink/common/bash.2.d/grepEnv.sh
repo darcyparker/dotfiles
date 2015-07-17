75886874dbeffed4674e37d8e646f80026932b75
Darcy Parker <darcyparker@gmail.com>
2015-07-17 16:01:30 -0400

    GREPOPTIONS is obsolete

--- common/bash.dot_symlink/common/bash.2.d/grepEnv.sh
+++ common/bash.dot_symlink/common/bash.2.d/grepEnv.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 #Note because of spaces, $GREP_OPTIONS must be wrapped in quotes
-if [ -z "$GREP_OPTIONS" ]; then
-  export GREP_OPTIONS="--binary-files=without-match --color=auto --exclude=tags --exclude=TAGS --exclude-dir=.git --exclude-dir=.svn --exclude-dir=.hg"
-fi
+#GREP_OPTIONS is deprecated
+# if [ -z "$GREP_OPTIONS" ]; then
+#   export GREP_OPTIONS="--binary-files=without-match --color=auto --exclude=tags --exclude=TAGS --exclude-dir=.git --exclude-dir=.svn --exclude-dir=.hg"
+# fi
--- common/bash.dot_symlink/common/bash.2.d/hubAlias.sh
+++ common/bash.dot_symlink/common/bash.2.d/hubAlias.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 #See https://github.com/github/hub
 if type hub &> /dev/null; then
   eval "$(hub alias -s)"
