ef22913d50a16f08b73e3206a0959d2f7294b226
Darcy Parker <darcyparker@gmail.com>
2015-07-21 08:38:35 -0400

    Addressed Shell Check issues in set_jdk_JAVA_HOME.sh script

--- common/bin.symlink_content/set_jdk_JAVA_HOME.sh
+++ common/bin.symlink_content/set_jdk_JAVA_HOME.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
-case "`uname`" in
+case "$(uname)" in
   CYGWIN*)
     #Assumes %JAVA_HOME% was set in Windows
     #Convert $JAVA_HOME to unix-like path
-    export JAVA_HOME=`cygpath --unix "$JAVA_HOME"`
+    JAVA_HOME=$(cygpath --unix "$JAVA_HOME")
     ;;
   Darwin*)
-    if [ -z $JAVA_HOME ]; then
+    if [ -z "$JAVA_HOME" ]; then
       if [ -x "/usr/libexec/java_home" ]; then
-        export JAVA_HOME=`/usr/libexec/java_home`
+        JAVA_HOME=$(/usr/libexec/java_home)
       elif [ -d "/System/Library/Frameworks/JavaVM.framework/Versions/CurrentJDK/Home" ]; then
         export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/CurrentJDK/Home
       fi
@@ -23,7 +23,7 @@ case "`uname`" in
       #Sometimes java is pointing to JRE inside a JDK
       # - if so, JAVA_HOME is set to the JDK root folder
       # - otherwise, JAVA_HOME is just 2 folders up from java command
-      export JAVA_HOME=`readlink -f $(which java) | sed 's:\(/jre\)\?/bin/java$:/:'`
+      JAVA_HOME=$(readlink -f "$(which java)" | sed 's:\(/jre\)\?/bin/java$:/:')
     else
       echo Warning: \$JAVA_HOME was already set. No change made.
     fi
@@ -31,7 +31,8 @@ case "`uname`" in
   MINGW*)
     #Assumes %JAVA_HOME% was set in Windows
     #Convert $JAVA_HOME to unix-like path
-    export JAVA_HOME="`(cd "$JAVA_HOME"; pwd)`"
+    JAVA_HOME="$(cd "$JAVA_HOME"; pwd)"
     ;;
 esac
-echo \$JAVA_HOME=$JAVA_HOME
+echo \$JAVA_HOME="$JAVA_HOME"
+export JAVA_HOME
