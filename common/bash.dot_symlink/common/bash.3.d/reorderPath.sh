cc25667c5fba17f1f92d7a204bfe14ff5f358b40
Darcy Parker <darcyparker@gmail.com>
2016-11-01 08:03:08 -0400

    New configs WIP

--- common/bash.dot_symlink/common/bash.3.d/reorderPath.sh
+++ common/bash.dot_symlink/common/bash.3.d/reorderPath.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 #Note: Surround $_frontPath in rare case there are spaces in it
 if [ -z "$_frontPath" ]; then
+  local _frontPathLocal
   # Move (or Add) /usr/local/bin to the front of the path
-  [ -d /usr/local/bin ] && _frontPath=/usr/local/bin
+  [ -d /usr/local/bin ] && _frontPathLocal=/usr/local/bin
 
   #THIS BLOCK is SLOW!!!
   # Move (or Add) npm's bin folder to the front of the path
@@ -10,21 +11,23 @@ if [ -z "$_frontPath" ]; then
   if [[ ${ENV_NAME} != "cygwin" ]] && [[ ${ENV_NAME} != "mingw64" ]] && [[ ${ENV_NAME} != "mingw32" ]]; then
     if type npm &>/dev/null; then
       _NPMPREFIXBIN=$(npm config get prefix)/bin
-      [ "$_NPMPREFIXBIN" != "/usr/local/bin" ] && _frontPath=${_NPMPREFIXBIN}:${_frontPath}
+      [ "$_NPMPREFIXBIN" != "/usr/local/bin" ] && _frontPathLocal=${_NPMPREFIXBIN}:${_frontPathLocal}
       unset _NPMPREFIXBIN
     fi
   fi
 
-  [ ! -z "$GOPATH" ] && _frontPath=${_frontPath}:${GOPATH}/bin
+  [ ! -z "$GOPATH" ] && _frontPathLocal=${_frontPathLocal}:${GOPATH}/bin
+
+  [ -d "$HOME/.cargo/bin" ] && _frontPathLocal=${HOME}/.cargo/bin:${_frontPathLocal}
 
   # Move (or Add) /usr/local/bin to the front of the path
-  [ -d "$HOME/.local/bin" ] && _frontPath=${HOME}/.local/bin:${_frontPath}
+  [ -d "$HOME/.local/bin" ] && _frontPathLocal=${HOME}/.local/bin:${_frontPathLocal}
 
   # if there is a bin folder in home, add it to front of  PATH
-  [ -d "$HOME/bin" ] && _frontPath=${HOME}/bin:${_frontPath}
+  [ -d "$HOME/bin" ] && _frontPathLocal=${HOME}/bin:${_frontPathLocal}
 
-  export _frontPath=$_frontPath;
+  export _frontPath="$_frontPathLocal"
 
   #For speed reasons, sed is called once at end
 fi
-export PATH=${_frontPath}:$(echo $PATH | sed -e "s;\(:\?/usr/local/bin\)|\(:\?$_NPMPREFIXBIN\)|\(:\?$HOME/.local/bin\)|\(:\?$HOME/bin\)|\(:\?$GOPATH/bin\);;")
+export PATH=${_frontPath}:$(echo $PATH | sed -e "s;\(:\?/usr/local/bin\)|\(:\?$_NPMPREFIXBIN\)|\(:\?$HOME/\.cargo/bin\)|\(:\?$HOME/\.local/bin\)|\(:\?$HOME/bin\)|\(:\?$GOPATH/bin\);;")
--- common/vim.dot_symlink/bundle_mine/my_plugin_configs/ftplugin/json/json.vim
+++ common/vim.dot_symlink/bundle_mine/my_plugin_configs/ftplugin/json/json.vim
@@ -1,2 +1,2 @@
 " Settings meant to be used with https://github.com/elzr/vim-json
-:setlocal foldmethod=syntax
+setlocal foldmethod=syntax
