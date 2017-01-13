2ebb08be42ab077ba3caef65dda5c24d7c83a306
Darcy Parker <darcyparker@gmail.com>
2017-01-13 17:33:28 -0500

    Update youCompleteMeBuild

--- common/vim.dot_symlink/youCompleteMeBuild.sh
+++ common/vim.dot_symlink/youCompleteMeBuild.sh
@@ -4,28 +4,31 @@ sudo apt-get update
 sudo apt-get install -y build-essential cmake
 sudo apt-get install -y python-dev python3-dev
 
+cd ~/.vim/bundle_python_github/youCompleteMe
+./install.py --tern-completer --racer-completer --gocode-completer
+
 #See https://github.com/Valloric/YouCompleteMe
-if [ ! -d "$HOME/src/ycm_build" ]; then
-  mkdir -p "$HOME/src/ycm_build"
-fi
-cd "$HOME/src/ycm_build"
-_clangDirName=$(find /usr/local -maxdepth 1 -type d -name "clang*" | sed -n -e "1p")
-echo "$_clangDirName"
-#Create generator files
-cmake -G "Unix Makefiles" -DPATH_TO_LLVM_ROOT="$_clangDirName" . "$HOME/.vim/bundle_python_github/youCompleteMe/third_party/ycmd/cpp"
-#compile libraries
-cmake --build . --target ycm_core --config Release
+# if [ ! -d "$HOME/src/ycm_build" ]; then
+#   mkdir -p "$HOME/src/ycm_build"
+# fi
+# cd "$HOME/src/ycm_build"
+# _clangDirName=$(find /usr/local -maxdepth 1 -type d -name "clang*" | sed -n -e "1p")
+# echo "$_clangDirName"
+# #Create generator files
+# cmake -G "Unix Makefiles" -DPATH_TO_LLVM_ROOT="$_clangDirName" . "$HOME/.vim/bundle_python_github/youCompleteMe/third_party/ycmd/cpp"
+# #compile libraries
+# cmake --build . --target ycm_core --config Release
 
-# Add go support
-cd "$HOME/.vim/bundle_python_github/youCompleteMe/third_party/ycmd/third_party/gocode"
-go build
+# # Add go support
+# cd "$HOME/.vim/bundle_python_github/youCompleteMe/third_party/ycmd/third_party/gocode"
+# go build
 
-#Add javascript support
-cd "$HOME/.vim/bundle_python_github/youCompleteMe/third_party/ycmd/third_party/tern_runtime"
-npm install --production
+# #Add javascript support
+# cd "$HOME/.vim/bundle_python_github/youCompleteMe/third_party/ycmd/third_party/tern_runtime"
+# npm install --production
 
-#Add rustc and cargo support
-cd "$HOME/.vim/bundle_python_github/youCompleteMe/third_party/ycmd/third_party/racerd"
-cargo build --release
+# #Add rustc and cargo support
+# cd "$HOME/.vim/bundle_python_github/youCompleteMe/third_party/ycmd/third_party/racerd"
+# cargo build --release
 
 popd > /dev/null
