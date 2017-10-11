d70637fb486f01335d7eebce2ca9211e52e790e5
Darcy Parker <darcyparker@gmail.com>
2017-10-11 01:01:27 -0400

    Fix nvm path issue

--- common/bash.dot_symlink/platform/linux/bash.2.d/nvm.sh
+++ common/bash.dot_symlink/platform/linux/bash.2.d/nvm.sh
@@ -5,4 +5,5 @@
 # Note: Has to be sourced for each shell because $NVM_DIR/nvm.sh does not seem
 # to save 100% of environment for child shells
 [ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh"  # load nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 [[ -s "$HOME/.avn/bin/avn.sh" ]] && source "$HOME/.avn/bin/avn.sh" # load avn
