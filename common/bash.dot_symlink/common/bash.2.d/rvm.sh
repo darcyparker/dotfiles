e2d8edb5fa15082b0ccf820a531cbe7b7758553f
Darcy Parker <darcyparker@gmail.com>
2018-03-13 15:26:08 -0400

    add rvm.sh initialization script

---
+++ common/bash.dot_symlink/common/bash.2.d/rvm.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#See https://rvm.io/
+if [ -s "$HOME/.rvm/scripts/rvm" ]; then
+  if ! declare -f rvm &>/dev/null ; then
+    # Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
+    export PATH="$PATH:$HOME/.rvm/bin"
+
+    # Load RVM into a shell session *as a function*
+    # shellcheck disable=SC1090
+    source "$HOME/.rvm/scripts/rvm"
+  fi
+fi
+
