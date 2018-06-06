98382cedf1a07d80f8c812310129e49cf16a4418
Darcy Parker <darcyparker@gmail.com>
2018-06-06 13:32:46 -0400

    common/bash.dot_symlink/platform/mingw32/bash.2.d/hpcalcFunc.sh: Fix shellcheck errors

--- common/bash.dot_symlink/platform/mingw32/bash.2.d/hpcalcFunc.sh
+++ common/bash.dot_symlink/platform/mingw32/bash.2.d/hpcalcFunc.sh
@@ -2,7 +2,7 @@
 #Start HP48GX emulator
 #For mingw64 and mingw32
 if ! declare -f hpcalc &>/dev/null ; then
-  if [ -e /c/Program\ Files\ \(x86\)/HP-Emulators/Emu48/Emu48.vbs -a -e /d/users/dparker/Documents/Hp48gx-darcy.E48 ]; then
+  if [ -e /c/Program\ Files\ \(x86\)/HP-Emulators/Emu48/Emu48.vbs ] && [ -e /d/users/dparker/Documents/Hp48gx-darcy.E48 ]; then
     function hpcalc {
       $(which cscript) 'c:\Program Files (x86)\HP-Emulators\Emu48\Emu48.vbs' 'd:\users\dparker\Documents\Hp48gx-darcy.E48' > /dev/null
     }
