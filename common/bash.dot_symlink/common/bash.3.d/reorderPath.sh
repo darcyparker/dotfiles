2cbf0baf91a2ea008ef90ff7879604a95a1a77e5
Darcy Parker <darcyparker@gmail.com>
2018-06-06 13:32:46 -0400

    common/bash.dot_symlink/common/bash.3.d/reorderPath.sh: Fix shellcheck errors

--- common/bash.dot_symlink/common/bash.3.d/reorderPath.sh
+++ common/bash.dot_symlink/common/bash.3.d/reorderPath.sh
@@ -1,35 +1,45 @@
 #!/usr/bin/env bash
 #Note: Surround $_frontPath in rare case there are spaces in it
 if [ -z "$_frontPath" ]; then
-  local _frontPathLocal
-  # Move (or Add) /usr/local/bin to the front of the path
-  [ -d /usr/local/bin ] && _frontPathLocal=/usr/local/bin
+  function setFrontPath {
+    local _frontPathLocal
+    # Move (or Add) /usr/local/bin to the front of the path
+    [ -d /usr/local/bin ] && _frontPathLocal=/usr/local/bin
 
-  #THIS BLOCK is SLOW!!!
-  # Move (or Add) npm's bin folder to the front of the path
-  # Often $NPMPREFIXBIN=/usr/local/bin, so I add this before $HOME/.local/bin
-  # if [[ ${ENV_NAME} != "cygwin" ]] && [[ ${ENV_NAME} != "mingw64" ]] && [[ ${ENV_NAME} != "mingw32" ]]; then
-  #   if type npm &>/dev/null; then
-  #     _NPMPREFIXBIN=$(npm config get prefix)/bin
-  #     [ "$_NPMPREFIXBIN" != "/usr/local/bin" ] && _frontPathLocal=${_NPMPREFIXBIN}:${_frontPathLocal}
-  #     unset _NPMPREFIXBIN
-  #   fi
-  # fi
+    #THIS BLOCK is SLOW!!!
+    # Move (or Add) npm's bin folder to the front of the path
+    # Often $NPMPREFIXBIN=/usr/local/bin, so I add this before $HOME/.local/bin
+    # if [[ ${ENV_NAME} != "cygwin" ]] && [[ ${ENV_NAME} != "mingw64" ]] && [[ ${ENV_NAME} != "mingw32" ]]; then
+    #   if type npm &>/dev/null; then
+    #     _NPMPREFIXBIN=$(npm config get prefix)/bin
+    #     [ "$_NPMPREFIXBIN" != "/usr/local/bin" ] && _frontPathLocal=${_NPMPREFIXBIN}:${_frontPathLocal}
+    #     unset _NPMPREFIXBIN
+    #   fi
+    # fi
 
-  [ -d /usr/local/go/bin ] && _frontPathLocal=/usr/local/go/bin:${_frontPathLocal}
+    [ -d /usr/local/go/bin ] && _frontPathLocal=/usr/local/go/bin:${_frontPathLocal}
 
-  [ ! -z "$GOPATH" ] && _frontPathLocal=${_frontPathLocal}:${GOPATH}/bin
+    [ ! -z "$GOPATH" ] && _frontPathLocal=${_frontPathLocal}:${GOPATH}/bin
 
-  [ -d "$HOME/.cargo/bin" ] && _frontPathLocal=${HOME}/.cargo/bin:${_frontPathLocal}
+    [ -d "$HOME/.cargo/bin" ] && _frontPathLocal=${HOME}/.cargo/bin:${_frontPathLocal}
 
-  # Move (or Add) /usr/local/bin to the front of the path
-  [ -d "$HOME/.local/bin" ] && _frontPathLocal=${HOME}/.local/bin:${_frontPathLocal}
+    # Move (or Add) /usr/local/bin to the front of the path
+    [ -d "$HOME/.local/bin" ] && _frontPathLocal=${HOME}/.local/bin:${_frontPathLocal}
 
-  # if there is a bin folder in home, add it to front of  PATH
-  [ -d "$HOME/bin" ] && _frontPathLocal=${HOME}/bin:${_frontPathLocal}
+    # if there is a bin folder in home, add it to front of  PATH
+    [ -d "$HOME/bin" ] && _frontPathLocal=${HOME}/bin:${_frontPathLocal}
 
-  export _frontPath="$_frontPathLocal"
+    export _frontPath="$_frontPathLocal"
+  }
+  setFrontPath
+  unset -f setFrontPath
 
   #For speed reasons, sed is called once at end
 fi
-export PATH=${_frontPath}:$(echo $PATH | sed -e "s;\(:\?/usr/local/bin\)|\(:\?$_NPMPREFIXBIN\)|\(:\?$HOME/\.cargo/bin\)|\(:\?$HOME/\.local/bin\)|\(:\?$HOME/bin\)|\(:\?$GOPATH/bin\);;")
+function updatePath {
+  local _removeExpression="s;\(:\?/usr/local/bin\)|\(:\?$_NPMPREFIXBIN\)|\(:\?$HOME/\.cargo/bin\)|\(:\?$HOME/\.local/bin\)|\(:\?$HOME/bin\)|\(:\?$GOPATH/bin\);;"
+  local _tailPath
+  _tailPath="$(echo "$PATH" | sed -e "${_removeExpression}")"
+  export PATH=${_frontPath}:${_tailPath}
+}
+updatePath
