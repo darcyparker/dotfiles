13deee56b142165506a192a7447852dc3329c830
Darcy Parker <darcyparker@gmail.com>
2018-06-06 13:32:46 -0400

    common/bin.symlink_content/sepCommits.sh: Fix shellcheck errors

--- common/bin.symlink_content/sepCommits.sh
+++ common/bin.symlink_content/sepCommits.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 main(){
-  git status --porcelain | while read _line; do
-    local _filePath=$(echo "$_line" | cut -d' ' -f2)
-    git add $_filePath
+  git status --porcelain | while read -r _line; do
+    local _filePath
+    _filePath=$(echo "$_line" | cut -d' ' -f2)
+    git add "$_filePath"
     git commit -m "Syntax updates/fixes for $_filePath"
   done
 }
