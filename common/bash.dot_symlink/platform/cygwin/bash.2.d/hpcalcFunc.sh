bd7afeb174e5b81bc58c7cb7b89fa2bcb6ab37cb
Darcy Parker <darcyparker@gmail.com>
2018-06-06 13:32:46 -0400

    common/bash.dot_symlink/platform/cygwin/bash.2.d/hpcalcFunc.sh: Fix shellcheck errors

--- common/bash.dot_symlink/platform/cygwin/bash.2.d/hpcalcFunc.sh
+++ common/bash.dot_symlink/platform/cygwin/bash.2.d/hpcalcFunc.sh
@@ -2,9 +2,9 @@
 #Start HP48GX emulator
 #For cygwin
 if ! declare -f hpcalc &>/dev/null ; then
-  if [ -e /cygdrive/c/Program\ Files\ \(x86\)/HP-Emulators/Emu48/Emu48.vbs -a -e /cygdrive/d/users/dparker/Documents/Hp48gx-darcy.E48 ]; then
+  if [ -e /cygdrive/c/Program\ Files\ \(x86\)/HP-Emulators/Emu48/Emu48.vbs ] && [ -e /cygdrive/d/users/dparker/Documents/Hp48gx-darcy.E48 ]; then
     function hpcalc {
-      `which cscript` 'c:\Program Files (x86)\HP-Emulators\Emu48\Emu48.vbs' 'd:\users\dparker\Documents\Hp48gx-darcy.E48' > /dev/null
+      $(which cscript) 'c:\Program Files (x86)\HP-Emulators\Emu48\Emu48.vbs' 'd:\users\dparker\Documents\Hp48gx-darcy.E48' > /dev/null
     }
     export -f hpcalc
   fi
