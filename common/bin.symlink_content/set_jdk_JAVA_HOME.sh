6a00761f10b0331de9a5dd7c613b3536833f7ff9
Darcy Parker <darcyparker@gmail.com>
2019-08-14 10:23:34 -0400

    set_jdk_JAVA_HOME.sh: Prefer command -v over which. https://stackoverflow.com/questions/592620/how-to-check-if-a-program-exists-from-a-bash-script

--- common/bin.symlink_content/set_jdk_JAVA_HOME.sh
+++ common/bin.symlink_content/set_jdk_JAVA_HOME.sh
@@ -23,7 +23,7 @@ case "$(uname)" in
       #Sometimes java is pointing to JRE inside a JDK
       # - if so, JAVA_HOME is set to the JDK root folder
       # - otherwise, JAVA_HOME is just 2 folders up from java command
-      JAVA_HOME=$(readlink -f "$(which java)" | sed 's:\(/jre\)\?/bin/java$:/:')
+      JAVA_HOME=$(readlink -f "$(command -v java)" | sed 's:\(/jre\)\?/bin/java$:/:')
     else
       echo Warning: \$JAVA_HOME was already set. No change made.
     fi
