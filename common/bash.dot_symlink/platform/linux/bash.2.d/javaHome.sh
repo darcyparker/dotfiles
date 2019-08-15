83c191689f0c71fd260be972e882130d6d8f1127
Darcy Parker <darcyparker@gmail.com>
2019-08-15 10:23:31 -0400

    javaHome.sh for linux: Use command -v

--- common/bash.dot_symlink/platform/linux/bash.2.d/javaHome.sh
+++ common/bash.dot_symlink/platform/linux/bash.2.d/javaHome.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-if [ -z "$JAVA_HOME" ]; then
+if [ -x "$(command -v java)" -a -z "$JAVA_HOME" ]; then
   #Not: this assumes gnu readlink (darwin and perhaps others use BSD
   #readlink where -f has different meaning)
   #Sometimes java is pointing to JRE inside a JDK
   # - if so, JAVA_HOME is set to the JDK root folder
   # - otherwise, JAVA_HOME is just 2 folders up from java command
-  JAVA_HOME=$(readlink -f "$(which java)" | sed 's:\(/jre\)\?/bin/java$::')
+  JAVA_HOME=$(readlink -f "$(command -v java)" | sed 's:\(/jre\)\?/bin/java$::')
   export JAVA_HOME
 fi
