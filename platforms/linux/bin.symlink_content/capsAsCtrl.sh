8564f42e516365f508169c92f1b0b72ffedcde60
Darcy Parker <darcyparker@gmail.com>
2019-09-22 21:26:07 -0400

    capsAsCtrl.sh: WIP

--- platforms/linux/bin.symlink_content/capsAsCtrl.sh
+++ platforms/linux/bin.symlink_content/capsAsCtrl.sh
@@ -1,23 +1,29 @@
 #!/usr/bin/env bash
-if [ -z "$_isNotCroutonEnv" ] && type croutonversion &>/dev/null; then
-  # For crouton (chromebook) chroot
-  # Swap the search key and ctrl key
-  if [ -z "$_croutonSwappedCtrl" ]; then
-    # Swap Ctrl and Search keys for ChromeOS Crouton chroots
-    # Note: This works well with the crouton 'keyboard' target
-    # Credit for this snippet: http://goo.gl/O8zaBR
-    # Note: Add this to xfce autostart so you don't have to
-    #       open a shell to swap the keys. See http://goo.gl/YZQcYO
+# Note: Add this to xfce autostart so you don't have to
+#       open a shell to swap the keys. See http://goo.gl/YZQcYO
 
-    # echo "Swapping Control and Search keys in Crouton"
-    xmodmap -e "keycode 133 = Control_L"
-    xmodmap -e "keycode 37 = Overlay1_Enable"
-    xmodmap -e "add control = Control_L"
-    xmodmap -e "remove control = Overlay1_Enable"
-    export _croutonSwappedCtrl=1
+if [ -z "$_capsAsCtrl" ]; then
+    if [[ $(uname -a) == *galliumos* ]]; then
+      # TODO
+      #For GalliumOS: keyboard settings are in
+      # /usr/share/X11/xkb/symbols/chromebook*
+      # See https://wiki.galliumos.org/Media_keys_and_default_keybindings for more info
+      echo "Swapping for galliumos todo"
+    else if type croutonversion &>/dev/null; then
+      # It is a chromebook (crouton)
+      # Swap the search key and ctrl key
+      # Note: This works well with the crouton 'keyboard' target
+      # Credit for this snippet: http://goo.gl/O8zaBR
+      echo "Swapping Control and Search keys in Crouton"
+      xmodmap -e "keycode 133 = Control_L"
+      xmodmap -e "keycode 37 = Overlay1_Enable"
+      xmodmap -e "add control = Control_L"
+      xmodmap -e "remove control = Overlay1_Enable"
+      export _capsAsCtrl=1
+    else
+      #It is not chromebook. We'll just make capslock ctrl key (search key does not apply, so no swap)
+      export _capsAsCtrl=1
+      setxkbmap -option ctrl:nocaps
+    fi
   fi
-else
-  #If not crouton, just make capslock ctrl key
-  export _isNotCroutonEnv=1
-  setxkbmap -option ctrl:nocaps
 fi
