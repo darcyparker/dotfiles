b2133d941ea7f320545282a1f6f63cc27144f687
Darcy Parker <darcyparker@gmail.com>
2019-12-13 10:15:34 -0500

    sepCommits.sh: Update

--- common/bin.symlink_content/sepCommits.sh
+++ common/bin.symlink_content/sepCommits.sh
@@ -2,9 +2,15 @@
 main(){
   git status --porcelain | while read -r _line; do
     local _filePath
+    local _fileName
+    # local _fileExtension
+    # local _baseFileName
     _filePath=$(echo "$_line" | cut -d' ' -f2)
+    _fileName=$(basename -- "$_filePath")
+    # _fileExtension="${_filename##*.}"
+    # _baseFileName="${_filename%.*}"
     git add "$_filePath"
-    git commit -m "Syntax updates/fixes for $_filePath"
+    git commit -m "$_fileName ($_filePath) updates"
   done
 }
 main
