959d5430a7dc99f578586975c854d04be867d6b7
Darcy Parker <darcyparker@gmail.com>
2020-05-08 20:55:57 -0400

    setup.sh: * Rework to load platforms based on isPlatform.sh results   * An environment could satisfy one or more platform * wrap work in functions and make vars local * Fix shellcheck errors.

--- setup.sh
+++ setup.sh
@@ -1,248 +1,335 @@
 #!/usr/bin/env bash
-# Script to add files from my_dotfiles repo to my $HOME (or target) folder
+# Script to add files from my_dotfiles repo to my $HOME (or target) directory
 # This script adds files as links or copies based on the content type.
 #
-# Files are organized into folders based on the environment (cygwin, darwin, linux, etc.)
-# A common folder is used to store files that are common to all environments.
-# The method for adding the dotfiles to my $HOME (or target) folder is indicated in the
-# extension add to the file or directory name.
+# Files are organized into directories:
+# * Based on the platform (cygwin, darwin, linux, msys, wsl, ...), and
+# * A common directory for files that are common to all platforms
 #
-# dot_symlink     : Target will be a symbolic link to source
-#                 : Target name will be prefixed with "." and ".dot_symlink" will be stripped
+# The copying/linking method is identified by a naming convention appended
+# to each file/directory's name:
 #
-# symlink         : Target will be a symbolic link to source
-#                 : Target name will only have the ".symlink" stripped
+# dot_symlink         : Target will be a symbolic link prefixed with `.` and 
+#                     : `.dot_symlink` will be stripped from name.
 #
-# template        : Target will be a copy of source
-#                 : Target name will only have the ".template" stripped
+# symlink_content     : A directory will be created with `.symlink_content` stripped from name
+#                     : and symbolic links of its content will created in this directory.
 #
-# dot_template    : Target will be a copy of source
-#                 : Target name will be prefixed with "." and ".dot_template" will be stripped
-#
-# symlink_content : A folder will be created with the target name
-#                 : and symbolic links of its content will be made in the folder
-#                 : Target name will have the ".symlink_content" stripped
-#                 : The symbolic links will not change the name.
-#
-# dot_symlink_content : A folder will be created with dot prefixed and
-#                     : and symbolic links of its content will be made in the folder
-#                     : Target name will have the ".dot_symlink_content" stripped
-#                     : The symbolic links will not change the name.
-#                     : Note, this is essentially same as `symlink_content` with a dot
-#                     : prefix added.
-#To do:
-# - call updatebundles.sh
-# - check if dirs/links made correctly. On cygwin, mklink must be run as administrator
+# dot_symlink_content : Similar to symlink_content, but the created directory will be prefixed
+#                     : with `.` .
 
+#_abspath(): evaluate the absolute path to a dir or
+_abspath() {
+  #if some pushd/popd fails, consume error message(s) and return exit status
+  if [[ -d "$1" ]]; then
+    pushd "$1" >/dev/null || return $?
+    pwd
+    popd > /dev/null || return $?
+  elif [[ -e $1 ]]; then
+    pushd "$(dirname "$1")" >/dev/null || return $?
+    echo "$(pwd)/$(basename "$1")"
+    popd > /dev/null || return $?
+  else
+    echo "$1" does not exist! >&2
+    return 127
+  fi
+}
 
-##########################################################################################
-# Function: _ln(SOURCE,TARGET)
-# - Creates symbolic link using command appropriate for OS
-##########################################################################################
+# _ln(SOURCE,TARGET)
+# * A wrapper around `ln` and `mklink` for creating symbolic links
 _ln() {
   local SOURCE=$1 ; local TARGET=$2
-  if [[ "$ENV_NAME" == "mingw32" || "$ENV_NAME" == "cygwin" ]] ; then
-    if [ -d "$SOURCE" ]; then
+  local _sourcePointsTo
+
+  if [ -n "$_isWindows" ] ; then
+    _sourcePointsTo=$(readlink -f "$SOURCE")
+    if [ -d "$_sourcePointsTo" ]; then
       #if a directory, then create a directory soft link
       echo "INFO: Adding symlink to dir \"$SOURCE\""
-      cygstart --action=runas cmd /c mklink /d "$(cygpath -a -w "$TARGET")" "$(cygpath -a -w "$SOURCE")"
+      if [ -x "$(command -v cygstart)" ]; then
+        # Note: mklink is a built in to cmd.exe, so this doesn't work from bash:
+        cygstart --action=runas cmd /c mklink /d "$(cygpath -a -w "$TARGET")" "$(cygpath -a -w "$SOURCE")"
+      else
+        #Could try: But need: ability to create links:
+        #https://github.com/git-for-windows/git/wiki/Symbolic-Links#allowing-non-administrators-to-create-symbolic-links
+        # mklink /d "$(cygpath -a -w "$TARGET")" "$(cygpath -a -w "$SOURCE")"
+        echo runas /user:Administrator cmd /c mklink /d "$(cygpath -a -w "$TARGET")" "$(cygpath -a -w "$SOURCE")"
+        echo "Must have cygstart to run mklink as admin"
+        exit 1;
+      fi
     else
       #If not a directory, then make a regular link
       echo "INFO: Adding symlink to file \"$SOURCE\""
-      cygstart --action=runas cmd /c mklink "$(cygpath -a -w "$TARGET")" "$(cygpath -a -w "$SOURCE")"
+      if [ -x "$(command -v cygstart)" ]; then
+        # Note: mklink is a built in to cmd.exe, so this doesn't work from bash:
+        cygstart --action=runas cmd /c mklink "$(cygpath -a -w "$TARGET")" "$(cygpath -a -w "$SOURCE")"
+      else
+        #Could try: But need: ability to create links:
+        #https://github.com/git-for-windows/git/wiki/Symbolic-Links#allowing-non-administrators-to-create-symbolic-links
+        echo runas /user:Administrator cmd /c mklink "$(cygpath -a -w "$TARGET")" "$(cygpath -a -w "$SOURCE")"
+        echo "Must have cygstart to run mklink as admin"
+        exit 1;
+      fi
     fi
   else
     echo "INFO: Adding symlink to \"$SOURCE\""
     ln -s "$SOURCE" "$TARGET"
   fi
+  #Useful test when debugging
+  #But don't use for cygwin because cygstart launches async work that may not be done yet
+  # if [ ! -L "$TARGET" ]; then
+  #   echo "Error creating sym link \"$TARGET\""
+  #   exit 1
+  # fi
 }
 
-##########################################################################################
-# Function: add_symlink(SOURCE,TARGET)
-# - Creates TARGET as a link to SOURCE
-##########################################################################################
-add_symlink() {
+# _addSymLink(SOURCE,TARGET)
+# * Creates TARGET as a link to SOURCE
+# * If an existing link exists at TARGET:
+#   * if it is a broken link, it deletes it
+#   * otherwise, it renames it before creating the new link
+_addSymLink() {
   local SOURCE=$1 ; local TARGET=$2
   TARGET=${TARGET/\/\//\/} #clean up double "/" in some target paths
+  local _sourcePointsTo
+  local _targetPointsTo
 
   #If $TARGET exits or it is a link (that may not exist), mv it
-  # if [ -e "$TARGET" -o -L "$TARGET" ]; then
   if [ -e "$TARGET" ]; then
-    local _itPointsTo=$(readlink "$TARGET")
-    #If $TARGET == $SOURCE, do nothing...
-    if  [[ $_itPointsTo != $SOURCE ]]; then
-      local _currentSecond=$(date +%s)
+    _targetPointsTo=$(readlink -f "$TARGET")
+    _sourcePointsTo=$(readlink -f "$SOURCE")
+    if  [[ $_targetPointsTo != "$_sourcePointsTo" ]]; then
+      local _currentSecond
+      _currentSecond=$(date +%s)
       echo "*** Warning: \"$TARGET\" already exists"
       echo "    and does not point to \"$SOURCE\"."
       echo "    Moving to \"${TARGET}.${_currentSecond}.old\"."
       mv "$TARGET" "${TARGET}.${_currentSecond}.old"
-      _ln "$SOURCE" "$TARGET"
+      _ln "$SOURCE" "$TARGET" || exit
     else
       echo "\"$TARGET\" already exists and links to this my_dotfiles repo."
     fi
   else
     if [ -L "$TARGET" ]; then
-      #TARGET is a link that does not exist (It is broken)
-      local _currentSecond=$(date +%s)
-      echo "*** Warning: \"$TARGET\" is a broken link"
-      echo "    Moving to \"${TARGET}.${_currentSecond}.brokenLink\"."
-      mv "$TARGET" "${TARGET}.${_currentSecond}.brokenLink"
-      _ln "$SOURCE" "$TARGET"
-    else
-      #The link does not exist and is not a broken link
-      #So create it.
-      _ln "$SOURCE" "$TARGET"
+      #$TARGET does not exit, but link does (so it is a bad link)
+      #So remove the bad link
+      rm "$TARGET"
     fi
+    #The link does not exist, so create it.
+    _ln "$SOURCE" "$TARGET" || exit
   fi
 }
 
-##########################################################################################
-# Function: add_it(type,source)
-# - Adds $SOURCE from the dot files repo to the desired target folder (usually $HOME)
-# - The way it adds $SOURCE depends on the $TYPE value
-#
-#   "mkdir_for_symlinks"  : makes a directory
-#   "dot_symlink"         : adds a symbolic link with name transformed to ".name"
-#   "symlink"             : adds a symbolic link (does not transform name)
-#   "symlink_content"     : adds a symbolic link (does not transform name)
-#                           (however it does strip the ".symlink_content" from its parent folder)
-#   "dot_symlink_content" : adds a symbolic link (does not transform name)
-#                           (however it does strip the ".dot_symlink_content" from its parent folder)
-#   "dot_template"        : Copies the template to the target with name transformed to ".name"
-#   "template"            : Copies the template to the target with no transform of the name
-##########################################################################################
-add_it() {
-  local SOURCE=$2 ; local TYPE=$1
-  local NAME=${SOURCE##*/} #extract filename or dirname from $SOURCE
-  local FROM_RELATIVE=${SOURCE#$FROM/} #strip $FROM from front of $SOURCE path
-  local TO_RELATIVE=${FROM_RELATIVE#*/} # strip first folder (such as common or platform)
-  local TO_RELATIVE=${TO_RELATIVE#$ENV_NAME/} # strip $ENV_NAME
-  local TO_RELATIVE=${TO_RELATIVE%$NAME} #strip $NAME from end
+# _addIt(TYPE, SOURCE)
+# * Adds $SOURCE from the dot files repo to the desired target folder (usually $HOME)
+# * The way it adds $SOURCE depends on the $TYPE value
+_addIt() {
+  local TYPE=$1
+  local SOURCE=$2
+  local TO=$3
+  local _BASENAME
+  local _DIRNAME
+  local TARGET
+  _BASENAME="$(basename "$SOURCE")"
 
   case "$TYPE" in
     "mkdir_for_symlinks")
-      TARGET=$TO/$TO_RELATIVE/${NAME%.symlink_content}
+      TARGET=$TO/${_BASENAME%.symlink_content}
       mkdir -p "$TARGET"
+      if [ ! -d "$TARGET" ]; then
+        echo "Error creating dir \"$TARGET\""
+        exit 1;
+      fi
       ;;
     "mkdir_for_dot_symlinks")
-      TARGET=$TO/$TO_RELATIVE/.${NAME%.dot_symlink_content}
+      TARGET=$TO/.${_BASENAME%.dot_symlink_content}
       mkdir -p "$TARGET"
+      if [ ! -d "$TARGET" ]; then
+        echo "Error creating dir \"$TARGET\""
+        exit 1;
+      fi
       ;;
     "dot_symlink")
-      TARGET=$TO/$TO_RELATIVE/.${NAME%.dot_symlink}
-      add_symlink "$SOURCE" "$TARGET"
-      ;;
-    "symlink")
-      TARGET=$TO/$TO_RELATIVE/${NAME%.symlink}
-      add_symlink "$SOURCE" "$TARGET"
+      TARGET=$TO/.${_BASENAME%.dot_symlink}
+      _addSymLink "$SOURCE" "$TARGET" || exit
       ;;
     "symlink_content")
-      TARGET=$TO/${TO_RELATIVE/.symlink_content/}/$NAME
-      add_symlink "$SOURCE" "$TARGET"
+      _DIRNAME="$(dirname "$SOURCE")"
+      _DIRNAME="$(basename "$_DIRNAME")"
+      _DIRNAME="${_DIRNAME/.symlink_content/}"
+      TARGET=$TO/$_DIRNAME/$_BASENAME
+      _addSymLink "$SOURCE" "$TARGET" || exit
       ;;
     "dot_symlink_content")
-      TARGET=$TO/.${TO_RELATIVE/.dot_symlink_content/}/$NAME
-      add_symlink "$SOURCE" "$TARGET"
+      _DIRNAME="$(dirname "$SOURCE")"
+      _DIRNAME="$(basename "$_DIRNAME")"
+      _DIRNAME=".${_DIRNAME/.dot_symlink_content/}"
+      TARGET=$TO/$_DIRNAME/$_BASENAME
+      _addSymLink "$SOURCE" "$TARGET" || exit
       ;;
-    "dot_template")
-      TARGET=$TO/$TO_RELATIVE/.${NAME%.dot_template}
-      echo "Creating \"$TARGET\" from template"
-      #To do: Why does prompt get skipped over?
-      cp -i "$SOURCE" "$TARGET"
-      ;;
-    "template")
-      TARGET=$TO/$TO_RELATIVE/${NAME%.template}
-      echo "Creating \"$TARGET\" from template"
-      #To do: Why does prompt get skipped over?
-      cp -i "$SOURCE" "$TARGET"
-      ;;
-    "font")
-      TARGET=$TO/.fonts/${NAME}
-      #echo Adding font \"$SOURCE\" \"$TARGET\"
-      add_symlink "$SOURCE" "$TARGET"
-      ;;
+    # Cases Not Used Today
+    # ====================
+    # "symlink")
+    #   TARGET=$TO/${_BASENAME%.symlink}
+    #   _addSymLink "$SOURCE" "$TARGET" || exit
+    #   ;;
+    # "dot_template")
+    #   TARGET=$TO/.${_BASENAME%.dot_template}
+    #   cp -i "$SOURCE" "$TARGET"
+    #   if [ ! -e "$TARGET" ]; then
+    #     echo "Error copying \"$SOURCE\" to \"$TARGET\""
+    #     exit 1
+    #   fi
+    #   ;;
+    # "template")
+    #   TARGET=$TO/${_BASENAME%.template}
+    #   cp -i "$SOURCE" "$TARGET"
+    #   if [ ! -e "$TARGET" ]; then
+    #     echo "Error copying \"$SOURCE\" to \"$TARGET\""
+    #     exit 1
+    #   fi
+    #   ;;
+    # "font")
+    #   TARGET=$TO/.fonts/${_BASENAME}
+    #   _addSymLink "$SOURCE" "$TARGET" || exit
+    #   ;;
     *)
-      echo "*** Error: TYPE \"$TYPE\" is unknown. Cannot add."
+      echo "*** Error: TYPE \"$TYPE\" is unknown. Cannot add \"$SOURCE\"."
+      exit 1
       ;;
   esac
 }
 
-##################
-# Body of script #
-##################
+# _addEachFrom(FROM, TO)
+# * Processes adding files from the FROM folder to the TO target
+_addEachFrom() {
+  local FROM="$1"
+  local TO="$2"
+  if [ -d "$FROM" ] && [ -d "$TO" ]; then
 
-# Identify environment (eg. cygwin, darwin, mingw32, linux)
-ENV_NAME=$(uname -s | tr '[:upper:]' '[:lower:]' | sed -e 's/_.*//')
+    local i
 
-# Identify target folder to setup
-if [ -d "$1" ]; then
-  TO="$(cd "$1"; echo "$PWD")"
-else
-  TO="$HOME"
-fi
+    echo "*** Create necessary folder structure if not present"
+    find "$FROM" -maxdepth 1 -type d -regex ".*[.]symlink_content$" | \
+      while read -r i; do
+        _addIt "mkdir_for_symlinks" "$i" "$TO" || exit
+      done
+    echo
+    find "$FROM" -maxdepth 1 -type d -regex ".*[.]dot_symlink_content$" | \
+      while read -r i; do
+        _addIt "mkdir_for_dot_symlinks" "$i" "$TO" || exit
+      done
+    echo
 
-# Identify folder to setup from
-thisFile=$(which "$0")
-FROM="$(cd "$(dirname "$thisFile")"; echo "$PWD")"
+    echo "*** Create symbolic links to content found in *.symlink_content folders"
+    find "$FROM" -maxdepth 2 -regex ".*[.]symlink_content/[^/]*" | \
+      while read -r i; do
+        _addIt "symlink_content" "$i" "$TO" || exit
+      done
+    echo
 
-echo "*** Create necessary folder structure if not present"
-find "$FROM/common" "$FROM/platforms/$ENV_NAME" -type d -regex ".*[.]symlink_content$" | \
-  while read i; do
-    add_it "mkdir_for_symlinks" "$i"
+    echo "*** Create symbolic links to content found in *.dot_symlink_content folders"
+    find "$FROM" -maxdepth 2 -regex ".*[.]dot_symlink_content/[^/]*" | \
+      while read -r i; do
+        _addIt "dot_symlink_content" "$i" "$TO" || exit
+      done
+    echo
+
+    echo "*** Create symbolic links to *.dot_symlink files"
+    find "$FROM" -maxdepth 1 -name "*.dot_symlink" | \
+      while read -r i; do
+        _addIt "dot_symlink" "$i" "$TO" || exit
+      done
+    echo
+
+    # Cases Not Used Today
+    # ====================
+    # echo "*** Create symbolic links to *.symlink files"
+    # find "$FROM" -maxdepth 1 -name "*.symlink" | \
+    #   while read -r i; do
+    #     _addIt "symlink" "$i" "$TO" || exit
+    #   done
+    # echo
+
+    # echo "*** Creating files from dot_templates"
+    # find "$FROM" -maxdepth 1 -name "*.dot_template" -type f | \
+    #   while read -r i; do
+    #     _addIt "dot_template" "$i" "$TO" || exit
+    #   done
+    # echo
+
+    # echo "*** Creating files from templates"
+    # find "$FROM" -maxdepth 1 -name "*.template" -type f | \
+    #   while read -r i; do
+    #     _addIt "template" "$i" "$TO" || exit
+    #   done
+    # echo
+  fi
+}
+
+# _updateVimBundles(TO)
+# * calls `$1/.vim/updateBundles.sh`
+_updateVimBundles() {
+  pushd . > /dev/null || exit 1
+  cd "$1/.vim" || exit 1
+  ./updateBundles.sh
+  popd > /dev/null || exit 1
+}
+
+# main(TO?)
+# * Sets up the required files in `TO` (defaults to `$HOME`)
+#   * Iterates over files to setup in `common` and `platforms`
+# * If the environment is Windows, also adds a symbolic link to
+#   `_vimrc`.
+# * Finally calls `_updateVimBundles`
+main() {
+  local TO
+  local FROM
+
+  # Identify target folder to setup
+  if [ -d "$1" ]; then
+    TO="$(_abspath "$1")"
+  else
+    TO="$HOME"
+  fi
+
+  #If needed create $TO/.config
+  mkdir -p "${XDG_CONFIG_HOME:=$TO/.config}"
+
+  # Identify folder to setup from
+  FROM="$(_abspath "$(dirname "$(command -v "$0")")")"
+
+  #Note: _isWindows is used by _ln() and unset when done
+  _isWindows="$("$FROM/platforms/cygwin/isPlatform.sh" || "$FROM/platforms/msys/isPlatform.sh" && echo 'true')"
+
+  #_addEachFrom common
+  _addEachFrom "$FROM/common" "$TO" || exit
+
+  #For each applicable platform, _addEachFrom
+  for i in "$FROM/platforms"/*; do
+    if [ -d "$i" ] && [ -x "$i/isPlatform.sh" ] && "$i/isPlatform.sh"; then
+      _addEachFrom "$i" "$TO" || exit
+    fi
   done
-echo
-find "$FROM/common" "$FROM/platforms/$ENV_NAME" -type d -regex ".*[.]dot_symlink_content$" | \
-  while read i; do
-    add_it "mkdir_for_dot_symlinks" "$i"
-  done
-echo
 
-echo "*** Create symbolic links to content found in *.symlink_content folders"
-find "$FROM/common" "$FROM/platforms/$ENV_NAME" -regex ".*[.]symlink_content/[^/]*" | \
-  while read i; do
-    add_it "symlink_content" "$i"
-  done
-echo
+  if [ -n "$_isWindows" ] ; then
+    echo "Linking \"$FROM/common/vimrc.dot_symlink\" to \"$TO/_vimrc\""
+    _addSymLink "$FROM/common/vimrc.dot_symlink" "$TO/_vimrc" || exit
+  fi
 
-echo "*** Create symbolic links to content found in *.dot_symlink_content folders"
-find "$FROM/common" "$FROM/platforms/$ENV_NAME" -regex ".*[.]dot_symlink_content/[^/]*" | \
-  while read i; do
-    add_it "dot_symlink_content" "$i"
-  done
-echo
+  _updateVimBundles "$TO" || exit
+}
 
-echo "*** Create symbolic links to *.dot_symlink files"
-find "$FROM/common" "$FROM/platforms/$ENV_NAME" -name "*.dot_symlink" | \
-  while read i; do
-    add_it "dot_symlink" "$i"
-  done
-echo
+main "$1"
 
-echo "*** Create symbolic links to *.symlink files"
-find "$FROM/common" "$FROM/platforms/$ENV_NAME" -name "*.symlink" | \
-  while read i; do
-    add_it "symlink" "$i"
-  done
-echo
-
-echo "*** Creating files from dot_templates"
-find "$FROM/common" "$FROM/platforms/$ENV_NAME" -name "*.dot_template" -type f | \
-  while read i; do
-    add_it "dot_template" "$i"
-  done
-echo
-
-echo "*** Creating files from templates"
-find "$FROM/common" "$FROM/platforms/$ENV_NAME" -name "*.template" -type f | \
-  while read i; do
-    add_it "template" "$i"
-  done
-echo
-
-pushd .
-cd ~/.vim
-./updateBundles.sh
-popd
+unset _isWindows
+unset -f main
+unset -f _addEachFrom
+unset -f _updateVimBundles
+unset -f _abspath
+unset -f _ln
+unset -f _addSymLink
+unset -f _addIt
 
 echo
 echo "Done setup of dotfiles!"
