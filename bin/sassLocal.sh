d09de204580f043d92c23c075db05ab480d55788
Darcy Parker <darcyparker@gmail.com>
2021-11-09 09:53:49 -0500

    bin/sassLocal.sh: Updated to look for dart-sass first.

--- bin/sassLocal.sh
+++ bin/sassLocal.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 main(){
   local _candidate
-  _candidate="$(npm bin)/node-sass"
+  _candidate="$(npm bin)/sass"
+  if [ ! -x "$_candidate" ]; then
+    _candidate="$(npm bin)/node-sass"
+  fi
+  if [ ! -x "$_candidate" ]; then
+    _candidate="$(npm bin -g)/sass"
+  fi
   if [ ! -x "$_candidate" ]; then
     _candidate="$(npm bin -g)/node-sass"
   fi
