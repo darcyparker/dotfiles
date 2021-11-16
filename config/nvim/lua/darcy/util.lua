161009c6dbd312784395bedee2711db598dea937
Darcy Parker <darcyparker@gmail.com>
2021-11-16 16:55:11 -0500

    util.lua: add description

--- config/nvim/lua/darcy/util.lua
+++ config/nvim/lua/darcy/util.lua
@@ -1,5 +1,7 @@
 local M = {}
 
+-- Unload a lua namespace
+-- Example: `require('darcy.config')` loads a lua script. and `unload_lua_namespace('darcy.config')` unloads/removes it.
 M['unload_lua_namespace'] = function(prefix)
   local prefix_with_dot = prefix .. '.'
   for key, value in pairs(package.loaded) do
