2b1ae7f2a4c2b87a3d27ba056325b1053fd0ffe2
Darcy Parker <darcy.parker@paperlessparts.com>
2021-12-07 21:59:11 -0500

    darwin wip

--- tag-Darwin/bash/bash.1.d/bash_darwin.sh
+++ tag-Darwin/bash/bash.1.d/bash_darwin.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
-# Aliases specific to OS X
 
-# which will look for aliases, functions and binaries
-# if [[ ! $(declare -F which) ]]; then
-#   function which { (alias; declare -f) | gwhich --tty-only --read-alias --read-functions --show-tilde --show-dot $@; }
-#   export -f which
-# fi
+#https://support.apple.com/kb/HT208050
+export BASH_SILENCE_DEPRECATION_WARNING=1
 
 #Note: Can't use $(brew --prefix) because brew is not in path yet
 #I know my brew prefix... so I am hard coding it for now
@@ -14,41 +10,28 @@ if [ -f /usr/local/etc/bash_completion ]; then
   . /usr/local/etc/bash_completion
 fi
 
+#TODO
+#if [ -x "$(command -v java)" ] && [ -z "$JAVA_HOME" ]; then
+#  if [ -x "/usr/libexec/java_home" ]; then
+#    JAVA_HOME="$(/usr/libexec/java_home)"
+#    export JAVA_HOME
+#  elif [ -d "/System/Library/Frameworks/JavaVM.framework/Versions/CurrentJDK/Home" ]; then
+#    export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/CurrentJDK/Home
+#  fi
+#fi
 
-if [ -x "$(command -v java)" ] && [ -z "$JAVA_HOME" ]; then
-  if [ -x "/usr/libexec/java_home" ]; then
-    JAVA_HOME="$(/usr/libexec/java_home)"
-    export JAVA_HOME
-  elif [ -d "/System/Library/Frameworks/JavaVM.framework/Versions/CurrentJDK/Home" ]; then
-    export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/CurrentJDK/Home
-  fi
-fi
+#TODO: Find better home
+# Setting PATH for Python 3.7
+# The original version is saved in .bash_profile.pysave
+PATH="/Library/Frameworks/Python.framework/Versions/3.7/bin:${PATH}"
+export PATH
 
-#MS Office Aliases
-# shellcheck disable=SC1117
-alias word="open -a Microsoft\ Word"
-# shellcheck disable=SC1117
-alias excel="open -a Microsoft\ Excel"
-# shellcheck disable=SC1117
-alias ppt="open -a Microsoft\ Powerpoint"
+#TODO: Find better home
+alias python="python3.7"
+alias pip="pip3.7"
 
-alias xcode="open -a Xcode.app"
-alias itunes="open -a iTunes.app"
-alias facetime="open -a facetime.app"
-
-# shellcheck disable=SC1117
-alias address="open -a Address\ Book"
-# shellcheck disable=SC1117
-alias activity="open -a Activity\ Monitor"
-alias textedit="open -a TextEdit.app"
-
-alias filezilla="open -a Filezilla"
 # shellcheck disable=SC1117
 alias chrome="open -a Google\ Chrome"
-alias firefox="open -a Firefox"
-alias mu="open -a muCommander"
-alias yed="open -a yEd"
-
 alias desktop="cd ~/Desktop"
 
 #copy the working directory into the clipboard
