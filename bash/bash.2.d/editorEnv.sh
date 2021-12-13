b88b3034ffef140a4e0455743407292df5967034
Darcy Parker <darcy.parker@paperlessparts.com>
2021-12-13 09:46:27 -0500

    editorEnv.sh: WIP

--- bash/bash.2.d/editorEnv.sh
+++ bash/bash.2.d/editorEnv.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 if [ -z "$EDITOR" ]; then
-  if type nvim &> /dev/null; then
-    EDITOR=nvim
-    alias vim=nvim
-  else
-    EDITOR=vim
-  fi
+  #if type nvim &> /dev/null; then
+  #  EDITOR=nvim
+  #  alias vim=nvim
+  #else
+  #  EDITOR=vim
+  #fi
+  EDITOR=vim
   export EDITOR
-else
-  if type nvim &> /dev/null; then
-    alias vim=nvim
-  fi
+#else
+#  if type nvim &> /dev/null; then
+#    alias vim=nvim
+#  fi
 fi
