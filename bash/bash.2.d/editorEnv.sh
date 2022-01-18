d6d09dd2881a23e05fbcb490cf1860c6ebb89490
Darcy Parker <945058+darcyparker@users.noreply.github.com>
2022-01-18 09:29:51 -0500

    nvim

--- bash/bash.2.d/editorEnv.sh
+++ bash/bash.2.d/editorEnv.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env bash
 if [ -z "$EDITOR" ]; then
-  #if type nvim &> /dev/null; then
-  #  EDITOR=nvim
-  #  alias vim=nvim
-  #else
-  #  EDITOR=vim
-  #fi
-  EDITOR=vim
+  if type nvim &> /dev/null; then
+    EDITOR=nvim
+  else
+    EDITOR=vim
+  fi
   export EDITOR
-#else
-#  if type nvim &> /dev/null; then
-#    alias vim=nvim
-#  fi
+fi
+if type nvim &> /dev/null; then
+  alias vim=nvim
 fi
