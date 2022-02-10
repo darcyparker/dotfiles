f33ebd6bc6cc4bb15594ee57259790424a64b444
Darcy Parker <945058+darcyparker@users.noreply.github.com>
2022-02-10 14:06:38 -0500

    wip

--- config/nvim/lua/darcy/lsp/null-ls.lua
+++ config/nvim/lua/darcy/lsp/null-ls.lua
@@ -7,6 +7,7 @@ end
 local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
+local code_actions = null_ls.builtins.code_actions
 
 null_ls.setup({
 	debug = false,
@@ -14,6 +15,10 @@ null_ls.setup({
 		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
 		formatting.black.with({ extra_args = { "--fast" } }),
 		formatting.stylua,
+
     -- diagnostics.flake8
+	  diagnostics.eslint_d,
+
+	  code_actions.eslint_d,
 	},
 })
