240f61689b6d98b4658c094382c7630d02af326f
Darcy Parker <945058+darcyparker@users.noreply.github.com>
2022-02-24 14:09:42 -0500

    add tsserver settings for lsp

--- config/nvim/lua/darcy/lsp/lsp-installer.lua
+++ config/nvim/lua/darcy/lsp/lsp-installer.lua
@@ -43,6 +43,11 @@ lsp_installer.on_server_ready(function(server)
     opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
   end
 
+  if server.name == "tsserver" then
+    local tsserver_opts = require("darcy.lsp.settings.tsserver")
+    opts = vim.tbl_deep_extend("force", tsserver_opts, opts)
+  end
+
   -- This setup() function is exactly the same as lspconfig's setup function.
   -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
   server:setup(opts)
--- config/nvim/lua/darcy/plugins.lua
+++ config/nvim/lua/darcy/plugins.lua
@@ -91,6 +91,7 @@ return packer.startup(function(use)
   use "williamboman/nvim-lsp-installer" -- simple to use language server installer
   use "tamago324/nlsp-settings.nvim" -- language server settings defined in json for
   use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
+  use "jose-elias-alvarez/nvim-lsp-ts-utils" -- for formatters and linters
 
   -- Telescope
   use "nvim-telescope/telescope.nvim"
