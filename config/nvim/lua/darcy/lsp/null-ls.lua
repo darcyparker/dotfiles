fba183385f82d2ff9c8dd45e17d13ac8e5bea8a6
Darcy Parker <945058+darcyparker@users.noreply.github.com>
2022-04-26 14:16:05 -0400

    null-ls updates

--- config/nvim/lua/darcy/lsp/null-ls.lua
+++ config/nvim/lua/darcy/lsp/null-ls.lua
@@ -4,21 +4,21 @@ if not null_ls_status_ok then
 end
 
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
-local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
-local diagnostics = null_ls.builtins.diagnostics
-local code_actions = null_ls.builtins.code_actions
 
 null_ls.setup({
 	debug = false,
 	sources = {
-		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
-		formatting.black.with({ extra_args = { "--fast" } }),
-		formatting.stylua,
+		null_ls.builtins.formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
+		null_ls.builtins.formatting.black.with({ extra_args = { "--fast" } }),
+		null_ls.builtins.formatting.stylua,
 
     -- diagnostics.flake8
-	  diagnostics.eslint_d,
+	  null_ls.builtins.diagnostics.eslint_d,
+	  null_ls.builtins.code_actions.eslint_d,
 
-	  code_actions.eslint_d,
+    null_ls.builtins.diagnostics.eslint.with({
+      prefer_local = "node_modules/.bin",
+    }),
 	},
 })
