5e091f90b123c767e6b5b9630b94eb9d36f6ff9b
Darcy Parker <945058+darcyparker@users.noreply.github.com>
2022-04-26 11:52:22 -0400

    Update nvim plugin configs

--- config/nvim/lua/darcy/setup/nvim-tree.lua
+++ config/nvim/lua/darcy/setup/nvim-tree.lua
@@ -1,6 +1,5 @@
 local g = vim.g
 g.nvim_tree_git_hl = 1 -- file highlight for git attributes
-g.nvim_tree_indent_markers = 1 -- indent markers when folders are open
 
 -- https://github.com/kyazdani42/nvim-tree.lua#setup
 require'nvim-tree'.setup {
@@ -17,6 +16,7 @@ require'nvim-tree'.setup {
       'fugitive:',
     }
   },
+  nvim_tree_indent_markers = 1,
   update_cwd = true,
   view = {
     auto_resize = true -- resize the tree to it's saved width when opening a new file
--- config/nvim/lua/darcy/setup/treesitter.lua
+++ config/nvim/lua/darcy/setup/treesitter.lua
@@ -4,7 +4,7 @@ if not status_ok then
 end
 
 configs.setup {
-  ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  ensure_installed = "all", -- A list of parser names, or "all"
   sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
   ignore_install = { "" }, -- List of parsers to ignore installing
   autopairs = {
