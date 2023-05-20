12ac4fe41d2f2e9120b3bdadf13725557d8679c5
Darcy Parker <945058+darcyparker@users.noreply.github.com>
2023-05-20 19:02:31 -0400

    editorEnv.sh update

--- bash/bash.2.d/editorEnv.sh
+++ bash/bash.2.d/editorEnv.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
-if [ -z "$EDITOR" ]; then
-  if type nvim &> /dev/null; then
-    ulimit -n 4096 #May increase in future.
-    EDITOR=nvim
-  else
-    EDITOR=vim
-  fi
-  export EDITOR
-fi
 if type nvim &> /dev/null; then
+  ulimit -n 4096 #May increase in future.
+  EDITOR=nvim
   alias vim=nvim
+else
+  EDITOR=vim
 fi
+export EDITOR
