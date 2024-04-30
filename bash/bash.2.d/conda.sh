3b60c0f1d76ba2f7761398a9b937c2127a534c00
Darcy Parker <945058+darcyparker@users.noreply.github.com>
2024-04-30 17:44:46 -0400

    conda

---
+++ bash/bash.2.d/conda.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/home/darcy/anaconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/home/darcy/anaconda3/etc/profile.d/conda.sh" ]; then
+        . "/home/darcy/anaconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/home/darcy/anaconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
