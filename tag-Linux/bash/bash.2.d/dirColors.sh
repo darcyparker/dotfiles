02ede98efc765be58fbd21c4384cc9a55a463099
Darcy Parker <945058+darcyparker@users.noreply.github.com>
2025-07-07 15:10:18 -0400

    improve dirColors

--- tag-Linux/bash/bash.2.d/dirColors.sh
+++ tag-Linux/bash/bash.2.d/dirColors.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
-eval "$(dircolors ~/.dircolors/dircolors.256dark)"
+if [ ! -f "$HOME/.dcolors.sh" ]; then
+  # If so, generate the script file. This only runs once.
+  dircolors "$HOME/.dircolors/dircolors.256dark" >"$HOME/.dcolors.sh"
+fi
+
+source "$HOME/.dcolors.sh"
