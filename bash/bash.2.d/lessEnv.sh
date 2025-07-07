87990f26548a63100e5ed45d31a6b18f04680587
Darcy Parker <945058+darcyparker@users.noreply.github.com>
2025-07-07 16:50:11 -0400

    update less env config

--- bash/bash.2.d/lessEnv.sh
+++ bash/bash.2.d/lessEnv.sh
@@ -1,30 +1,11 @@
 #!/usr/bin/env bash
-if [ -z "$_lessEnv" ]; then
-  export _lessEnv=1
-  export PAGER="less"
 
-  #less default behaviour
-  #* -F -X Don't paginate if < 1 page
-  export LESS="-F -X ReMqXi~g"
+# Set less as the default pager for commands like 'man'
+export PAGER="less"
 
-
-  #R Raw color codes in output (don't remove color codes)
-  #e exit the second time it reaches end of line
-  #M verbose prompt
-  #q quiet operation
-  #X disables sending the termcap initialization/deinitialization strings
-  #  ie prevents clear screen
-  #i searches ignore case
-  #~ - Don't show those weird ~ symbols on lines after EOF
-  #g - Highlight results when searching with slash key (/)
-
-  # Colored man pages: http://linuxtidbits.wordpress.com/2009/03/23/less-colors-for-man-pages/
-  # Less Colors for Man Pages
-  export LESS_TERMCAP_mb=$'\E[01;31m'       # begin blinking
-  export LESS_TERMCAP_md=$'\E[01;38;5;74m'  # begin bold
-  export LESS_TERMCAP_me=$'\E[0m'           # end mode
-  export LESS_TERMCAP_se=$'\E[0m'           # end standout-mode
-  export LESS_TERMCAP_so=$'\E[38;5;016m\E[48;5;220m'    # begin standout-mode - info box
-  export LESS_TERMCAP_ue=$'\E[0m'           # end underline
-  export LESS_TERMCAP_us=$'\E[04;38;5;146m' # begin underline
-fi
+# Set default options for less
+# -F : Quit if content fits on one screen.
+# -R : Output raw control characters (for colors).
+# -i : Case-insensitive searching.
+# -X : Do not clear the screen on exit.
+export LESS="-FRiX"
