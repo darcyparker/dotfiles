697bf48fdbb41b0c9f210b3a7429101e3af72ff2
Darcy Parker <945058+darcyparker@users.noreply.github.com>
2026-01-20 21:24:19 -0500

    add fn to adjust pacman mirrors

--- tag-Linux/bash/bash.2.d/linuxAliases.sh
+++ tag-Linux/bash/bash.2.d/linuxAliases.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Aliases specific to Linux
-if type exa &> /dev/null; then
+if type exa &>/dev/null; then
   alias ls="exa -F"
   alias ll="exa -lhF"
   alias lt="exa -tlAhF"
@@ -12,4 +12,11 @@ else
   alias lt="ls --color -tlAh"
   alias lal="ls --color -lAh"
   alias dir="ls --color -lAh"
+fi
+if command -v pacman >/dev/null 2>&1; then
+  # optimize downloads of pacman (finds 5 fastest mirrors before doing update)
+  fix-speed() {
+    sudo pacman-mirrors --fasttrack 5 && sudo pacman -Syyu
+  }
+  export -f fix-speed
 fi
